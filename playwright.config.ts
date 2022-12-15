63f18423d46982726166d698429d85727aa5bcae
azmansabbir <unidevgo.qa1@gmail.com>
2022-12-15 18:52:29 +0600

    Updated Code

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
+const { publish, defineConfig } = require('test-results-reporter');
 import { devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
@@ -27,6 +28,28 @@ const config: PlaywrightTestConfig = {
   workers: process.env.CI ? 1 : 1,
 
   // reporter: [['test-results-reporter',  {outputFile: "results.xml"}]],
+  
+  // reporter: process.env.CI
+  // ? [[
+  //     {
+  //     "targets": [
+  //       {
+  //         "name": "teams",
+  //         "inputs": {
+  //           "url": "https://transportedlabs.webhook.office.com/webhookb2/1af1ca62-123b-4948-9a2f-f84e4cb589e6@fee95589-ad46-417d-9636-8b4078359ad9/IncomingWebhook/dbccaaa1799743e38ed4d57ba2b562bc/a6b92662-3ec5-443a-8d4e-10db06594a3c"
+  //         }
+  //       }
+  //     ],
+  //     "results": [
+  //       {
+  //         "type": "testng",
+  //         "files": ["./results.xml"]
+  //       }
+  //     ]
+  //   }
+  // ]],
+    
+  
   
   reporter: [["junit", {
     outputFile: "results.xml"
